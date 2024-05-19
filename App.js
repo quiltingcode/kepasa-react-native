a7f4e59472c8ee2674317865b02de4b8c60fc78c
Kelly Hutchison <106617070+quiltingcode@users.noreply.github.com>
2024-05-19 19:45:49 +0000

    copy across focustime setup

--- App.js
+++ App.js
@@ -1,16 +1,28 @@
-import { StatusBar } from "expo-status-bar";
+import { StatusBar as ExpoStatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, View } from "react-native";
+import {
+  StatusBar,
+  StyleSheet,
+  SafeAreaView,
+  Text,
+  View,
+  Platform,
+} from "react-native";
 
 export default function App() {
-    return (
-        <View>
-
+  return (
+    <>
+      <SafeAreaView style={{ flex: 1, marginTop: StatusBar.currentHeight }}>
+        <View style={{ padding: 16, backgroundColor: "green" }}>
+          <Text>search</Text>
         </View>
-    );
-
+        <View style={{ flex: 1, padding: 16, backgroundColor: "blue" }}>
+          <Text>list</Text>
+        </View>
+      </SafeAreaView>
+      <ExpoStatusBar style="auto" />
+    </>
+  );
 }
 
-const styles = StyleSheet.create({
-    
-})
+const styles = StyleSheet.create({});
