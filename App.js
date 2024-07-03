030700e393f87b48e47e5f10d687e2eb96dd1795
Kelly Hutchison <106617070+quiltingcode@users.noreply.github.com>
2024-07-03 15:08:37 +0000

    Add event pages structure with list section

--- App.js
+++ App.js
@@ -1,38 +1,12 @@
 import { StatusBar as ExpoStatusBar } from "expo-status-bar";
 import React from "react";
-import {
-  StatusBar,
-  StyleSheet,
-  SafeAreaView,
-  Text,
-  View,
-  Platform,
-} from "react-native";
-import EventsPage from "./src/pages/EventsPage";
+import { EventsScreen } from "./src/features/screens/event.screen";
 
 export default function App() {
   return (
     <>
-      <SafeAreaView style={styles.container}>
-        <EventsPage />
-      </SafeAreaView>
+      <EventsScreen />
       <ExpoStatusBar style="auto" />
     </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    marginTop: StatusBar.currentHeight
-  },
-  search: {
-    padding: 16,
-    backgroundColor: "yellow"
-  },
-  list: {
-    flex: 1,
-    padding: 16,
-    backgroundColor: "blue"
-  }
-});
