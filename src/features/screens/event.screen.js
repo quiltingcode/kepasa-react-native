c506dd79e1fb70d67b1e1951c9146d01409f01c3
Kelly Hutchison <106617070+quiltingcode@users.noreply.github.com>
2024-07-04 16:33:04 +0000

    Convert events page to styled components

--- src/features/screens/event.screen.js
+++ src/features/screens/event.screen.js
@@ -1,30 +1,31 @@
 import React from "react";
+import styled from "styled-components/native";
 import { Searchbar } from "react-native-paper";
-import { StatusBar, StyleSheet, SafeAreaView, Text, View } from "react-native";
+import { StatusBar } from "react-native";
 import { EventCard } from "../events/components/event-card";
 
-export const EventsScreen = () => (
-  <SafeAreaView style={styles.container}>
-    <View style={styles.search}>
-      <Searchbar />
-    </View>
-    <View style={styles.list}>
-      <EventCard />
-    </View>
-  </SafeAreaView>
-);
+const SafeArea = styled.SafeAreaView`
+  flex: 1;
+  margin-top: ${StatusBar.currentHeight}px;
+`;
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    marginTop: StatusBar.currentHeight,
-  },
-  search: {
-    padding: 16,
-  },
-  list: {
-    flex: 1,
-    padding: 16,
-    backgroundColor: "powderblue",
-  },
-});
+const SearchContainer = styled.View`
+  padding: 16px;
+`;
+
+const EventListContainer = styled.View`
+  flex: 1;
+  padding: 16px;
+  background-color: powderblue;
+`;
+
+export const EventsScreen = () => (
+  <SafeArea>
+    <SearchContainer>
+      <Searchbar />
+    </SearchContainer>
+    <EventListContainer>
+      <EventCard />
+    </EventListContainer>
+  </SafeArea>
+);
