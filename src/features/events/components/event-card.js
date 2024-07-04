518d908fcfe44fa656bceed6c86576c765596d21
Kelly Hutchison <106617070+quiltingcode@users.noreply.github.com>
2024-07-04 15:58:56 +0000

    Install styled components

--- src/features/events/components/event-card.js
+++ src/features/events/components/event-card.js
@@ -1,20 +1,54 @@
 import React from "react";
-import { Text, StyleSheet, Image, View } from "react-native";
+import styled from "styled-components/native";
 import { Avatar, Card, IconButton, MD3Colors } from "react-native-paper";
+import { StatusBar, StyleSheet, SafeAreaView, Text, View } from "react-native";
+
+// Styled components
+const StyledCard = styled(Card)`
+  background-color: white;
+`;
+
+const CardTitle = styled.View`
+  margin-left: 8px;
+  margin-top: 8px;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const OwnerContainer = styled.View`
+  flex-direction: row;
+  align-items: center;
+`;
+
+const AvatarImage = styled(Avatar.Image)`
+  margin-right: 8px;
+`;
+
+const CardCover = styled(Card.Cover)`
+  padding: 10px;
+  background-color: white;
+`;
+
+const CardContent = styled(Card.Content)`
+  padding: 5px;
+  align-items: center;
+`;
+
+const EventInfo = styled.View`
+  flex-direction: row;
+  align-items: center;
+`;
+
+const UpdatedText = styled.Text`
+  margin-right: 8px;
+`;
 
 export const EventCard = ({ event = {} }) => {
   const {
-    id,
     owner = "Kelly",
-    profile_id,
     profile_image = "https://res.cloudinary.com/dkolsfjkx/image/upload/v1679395009/default_profile_veem15.jpg",
-    comments_count,
-    interested_count,
-    going_count,
-    interested_id,
-    going_id,
     title = "great event",
-    description = "A  brilliant event",
     event_date = "12 August 2024",
     tags = "musical",
     image = "https://res.cloudinary.com/dkolsfjkx/image/upload/v1685551309/media/images/lion_king_kqqaoc.jpg",
@@ -22,34 +56,31 @@ export const EventCard = ({ event = {} }) => {
   } = event;
 
   return (
-    <Card elevation={5} style={styles.card}>
-      <View style={styles.cardTitle}>
-        <View style={styles.leftContainer}>
-          <Avatar.Image
+    <StyledCard elevation={5}>
+      <CardTitle>
+        <OwnerContainer>
+          <AvatarImage
             size={32}
             source={{
               uri: profile_image,
             }}
-            style={styles.avatar}
           />
           <Text>{owner}</Text>
-        </View>
-        <View style={styles.rightContainer}>
-          <Text>{updated_at}</Text>
-        </View>
-      </View>
+        </OwnerContainer>
+        <UpdatedText>{updated_at}</UpdatedText>
+      </CardTitle>
 
-      <Card.Cover key={title} source={{ uri: image }} style={styles.cover} />
-      <Card.Content style={styles.cardContent}>
-        <View style={styles.eventInfo}>
+      <CardCover key={title} source={{ uri: image }} />
+      <CardContent>
+        <EventInfo>
           <Text>
             {title} - {event_date}
           </Text>
-        </View>
+        </EventInfo>
         <View>
           <Text>{tags}</Text>
         </View>
-        <View style={styles.eventInfo}>
+        <EventInfo>
           <IconButton
             icon="eye"
             iconColor={MD3Colors.success50}
@@ -68,27 +99,10 @@ export const EventCard = ({ event = {} }) => {
             size={25}
             onPress={() => console.log("Pressed")}
           />
-        </View>
-      </Card.Content>
-    </Card>
+        </EventInfo>
+      </CardContent>
+    </StyledCard>
   );
 };
 
-const styles = StyleSheet.create({
-  card: { backgroundColor: "white" },
-  cover: { padding: 10, backgroundColor: "white" },
-  avatar: { marginRight: 8 },
-  cardTitle: {
-    padding: 5,
-    flexDirection: "row",
-    justifyContent: "space-between",
-    alignItems: "center",
-  },
-  cardContent: {
-    padding: 5,
-    alignItems: "center",
-  },
-  leftContainer: { flexDirection: "row", alignItems: "center" },
-  rightContainer: { marginRight: 8 },
-  eventInfo: { flexDirection: "row", alignItems: "center" },
-});
+export default EventCard;
